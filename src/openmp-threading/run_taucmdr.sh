25deefb56fc1287e0e8557dd42f1055184b7be3b
Brian Joseph Gravelle <gravelle@lanl.gov>
2020-08-14 14:20:50 -0600

    saving changes before master merge

--- src/openmp-threading/run_taucmdr.sh
+++ src/openmp-threading/run_taucmdr.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #
-#SBATCH --job-name=snap_tau
+#SBATCH --job-name=xsbench_tau
 #SBATCH --output=test_tau.txt
 #
 #SBATCH --ntasks=40
 #SBATCH --time=8:00:00
-#SBTACH -p skylake-gold
 
 source /projects/artab/soft/taucmdr-arm.env
 module load openmpi/3.1.3-gcc_8.2.0
 module load cmake
 
 # parameters for TAU
-EXP=event_overview_8p56t  # experiment name - must be created before running this script
+#EXP=history_unroll_8p56t  # experiment name - must be created before running this script
+#EXP=history_overview_8p56t
+EXP=event_k1_overview_8p56t
 tau experiment select $EXP # switch tau to this experiment
 
 export OMPI_MCA_pml=ob1
@@ -24,11 +25,12 @@ export OMP_PROC_BIND=close
 # command to execute the mkFit program
 #    note the '\' before each space
 #    also this must be redefined inside the loop to overwrite the variable as they vary
-CMD="mpirun -n 8 ./XSBench -m event -t 56"
+CMD="mpirun -n 4 ./XSBench -m event -t 56"
+#CMD="mpirun -n 4 ./XSBench -t 56"
 
 # measurement list; these are tau commander measurements that must be created first
-measure_list=(fp_ins simd_ins tot_cyc tot_ins ld_ins sr_ins br_ins dcm1 dca1 tcm2 tca2 mem_rd mem_wr stl_front stl_back)
-
+measure_list=(spec_ins fp_ins simd_ins tot_cyc tot_ins ld_ins sr_ins br_ins dcm1 dca1 tcm2 tca2 mem_rd mem_wr stl_front stl_back)
+#measure_list=(dca1 tcm2 tca2 mem_rd mem_wr stl_front stl_back)
 function run_trials {
 
 	for i in ${measure_list[@]}; do
